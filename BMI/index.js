c22bf16688e26f548877842884993c9fb0ce05d7
omoladeajala <shadowcoderx.02@gmail.com>
2026-04-29 20:49:50 +0100

    Update title in index.html to "Love Calculator" and comment out BMI calculation code in index.js

--- BMI/index.js
+++ BMI/index.js
@@ -1,7 +1,14 @@
-function calcBmi(weight, height) {
-  var bmi = weight / Math.pow(height, 2);
+// function calcBmi(weight, height) {
+//   var bmi = weight / Math.pow(height, 2);
 
-  return Math.round(bmi);
-}
-var bmi = calcBmi(65, 1.8);
-console.log(bmi);
+//   return Math.round(bmi);
+// }
+// var bmi = calcBmi(65, 1.8);
+// console.log(bmi);
+
+prompt("What is your name?");
+prompt("What is their name?");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+alert("Your love score is " + loveScore + "%");
