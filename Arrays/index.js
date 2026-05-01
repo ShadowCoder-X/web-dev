3ee90d98a1ca63d77ea7904d361d2d62c4b710f4
omoladeajala <shadowcoderx.02@gmail.com>
2026-05-01 22:38:55 +0100

    Comment out guest list functionality in index.js

--- Arrays/index.js
+++ Arrays/index.js
@@ -1,9 +1,19 @@
-var guestList = ["Angel", " Grace", "Peter", "Paul", "Tomi"];
+// var guestList = ["Angel", " Grace", "Peter", "Paul", "Tomi"];
 
-var guestName = prompt("What is the name of your guest?");
+// var guestName = prompt("What is the name of your guest?");
 
-if (guestList.includes(guestName)) {
-  alert("You are welcome!");
-} else {
-  alert("You are not welcome here.");
+// if (guestList.includes(guestName)) {
+//   alert("You are welcome!");
+// } else {
+//   alert("You are not welcome here.");
+// }
+
+function whosPlaying(names) {
+  var numberOfPeople = names.length;
+  var randomPersonPosition = Math.floor(Math.random() * numberOfPeople);
+  var randomPerson = names[randomPersonPosition];
+
+  console.log(randomPerson + " is going to buy lunch today!");
 }
+
+var whosPlaying = ["Micheal", "Rita", "John", "Paul", "Omoh"];
