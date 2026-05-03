33c0e3c4f3e1b8265d011d0b533702e3af77d35f
omoladeajala <shadowcoderx.02@gmail.com>
2026-05-03 18:48:23 +0100

    Add initial implementation of index.html and index.js with basic structure and comments

---
+++ DOM/index.js
@@ -0,0 +1,22 @@
+// document
+
+// #document (html...)
+// document.firstElementChild.lastElementChild
+
+// <body>​…​</body>​
+
+// const heading = document.firstElementChild.lastElementChild.firstElementChild;
+
+// undefined
+// heading
+
+// <h1>​Hello​</h1>​
+// heading.style.color = "red;
+
+// VM1446:1 Uncaught SyntaxError: Invalid or unexpected tokenUnderstand this error
+
+// heading.style.color = "red";
+
+// 'red'
+
+// document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Omolade";
