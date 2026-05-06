0b42940e15ff7199cd254dd6649e4c365f0d474b
omoladeajala <shadowcoderx.02@gmail.com>
2026-05-06 22:37:50 +0100

    Refactor code structure for improved readability and maintainability

--- Dicee/index.js
+++ Dicee/index.js
@@ -1,3 +1,9 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
 var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
